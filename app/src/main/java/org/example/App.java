ec1564b1807b415ce63258e1a9430d3a6218d276
jsb999 <90780236+jsb999@users.noreply.github.com>
2025-09-15 15:22:05 +0000

    finished code

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,8 +1,39 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class App {
   public static void main(String[] args) {
-    System.out.println("You can put the driver program for your Payroll Calculator here!");
-    System.out.println("Feel free to create other files, as needed, to house your code.");
-  }
+    Scanner scanner = new Scanner(System.in);
+    double hoursUserWorked = 0;
+    int userDependents = 0;
+
+    System.out.println("Welcome to the payroll calculator!");
+    System.out.println("----------------------------------");
+    System.out.println("Please enter the number of hours/nyou have worked this week:");
+    hoursUserWorked = scanner.nextDouble();
+    System.out.println("How many dependents do you have?");
+    userDependents = scanner.nextInt();
+
+    Payroll payroll = new Payroll(hoursUserWorked, userDependents);
+    System.out.println("----------------------------------");
+    System.out.println("Here is your payroll information:");
+    System.out.println("you have worked " + payroll.getHours() + " hours at $" + Payroll.EMPLOYEE_HOURLY_PAY + " per hour.");
+    System.out.println("Your gross pay is: $" + payroll.getGrossRate());
+    System.out.println("Your net pay is: $" + payroll.getUserNetPay());
+    
+    System.out.println("\n");
+    
+    System.out.println("you payed " + payroll.getExpenses() + " dollars for your expenses.");
+    System.out.println("you payed " + Payroll.UNION_DUES_DOLLARS + " dollars for your union dues.\n");
+    
+    System.out.println("you payed " + Payroll.SS_TAX_PERCENT + "% for your social security tax. for a total of " + (payroll.getGrossRate()*Payroll.SS_TAX_PERCENT)/100 + " dollars.");
+    System.out.println("you payed " + Payroll.FEDERAL_INCOME_TAX_PERCENT + "% for your federal income tax. for a total of " + (payroll.getGrossRate()*Payroll.FEDERAL_INCOME_TAX_PERCENT)/100 + " dollars.");
+    System.out.println("you payed " + Payroll.STATE_INCOME_TAX_PERCENT + "% for your state income tax. for a total of " + (payroll.getGrossRate()*Payroll.STATE_INCOME_TAX_PERCENT)/100 + " dollars.");
+    System.out.println("\n");
+    
+    System.out.println("----------------------------------");
+    System.out.println("Thank you for using the payroll calculator!");
+    
+  }  
 }
---
+++ app/src/main/java/org/example/Payroll.java
@@ -0,0 +1,55 @@
+package org.example;
+
+public class Payroll{
+    private double hoursUserWorked;
+    private int userDependents;
+    private double userGrossPay;
+    private double userNetPay;
+
+    public static final double EMPLOYEE_HOURLY_PAY = 16.78;
+    public static final double OVERTIME_RATE = 1.5;
+    public static final double SS_TAX_PERCENT = 6;
+    public static final double FEDERAL_INCOME_TAX_PERCENT = 14;
+    public static final double STATE_INCOME_TAX_PERCENT = 5;
+    public static final double UNION_DUES_DOLLARS = 10;
+
+    public Payroll(double hoursUserWorked_,int userDependents_){
+        this.hoursUserWorked = hoursUserWorked_;
+        this.userDependents = userDependents_;
+    }
+
+    public double getHours(){
+        return this.hoursUserWorked;
+    }
+    
+
+    public double getGrossRate(){
+        userGrossPay = 0;
+        if(hoursUserWorked >40){
+            userGrossPay += (hoursUserWorked-40)*(EMPLOYEE_HOURLY_PAY*OVERTIME_RATE);
+            userGrossPay += 40*EMPLOYEE_HOURLY_PAY;
+        } else {
+            userGrossPay += hoursUserWorked*EMPLOYEE_HOURLY_PAY;
+        }
+
+        return userGrossPay;
+    }
+
+    public double getUserNetPay(){
+        userNetPay = userGrossPay;
+        userNetPay -= (userGrossPay*SS_TAX_PERCENT)/100;
+        userNetPay -= (userGrossPay*FEDERAL_INCOME_TAX_PERCENT)/100;
+        userNetPay -= (userGrossPay*STATE_INCOME_TAX_PERCENT)/100;
+        userNetPay -= UNION_DUES_DOLLARS;
+        userNetPay -= getExpenses();
+        return userNetPay;
+    }
+
+    public double getExpenses(){
+        if(userDependents>=3){
+            return 35;
+        } else{
+            return 15;
+        }
+    }
+}
